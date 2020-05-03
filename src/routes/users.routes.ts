99105367f05e68fb05372e669b52253cc0979a8c
Lara <laraamori.machado@gmail.com>
2020-05-03 18:53:20 -0300

    image upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
@@ -22,6 +24,10 @@ usersRouter.post('/', async (request, response) => {
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
+  return response.json({ ok: true });
 });
 
 export default usersRouter;
