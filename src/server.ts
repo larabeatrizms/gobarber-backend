34c5e5c5cfbaa90b50900a4b6842a43b7b2a84f9
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-21 16:02:59 -0300

    adding cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -11,6 +12,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
@@ -32,5 +34,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('🚀️  Server started on port 3333 ');
+  console.log('🚀️ Server started on port 3333 ');
 });
