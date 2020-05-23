1fb1045ee384175785821e46ca1c4221d40ccfbf
Lara Beatriz <laraamori.machado@gmail.com>
2020-05-23 16:34:52 -0300

    :wrench: thinking about the tests

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,11 @@
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointnent', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+  // it('should not be able to create two appointnents on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
+});
